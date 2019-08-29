f5582adb9bf0aac3685bcd4415630407083f09f3
su <recallsufuture@gmail.com>
2019-08-29 18:29:23 +0800

    完成我的煤医的控制器

---
+++ src/app/controller/myncmc.ts
@@ -0,0 +1,51 @@
+import { Context, inject, controller, post, provide, get } from 'midway';
+import { cookieJar } from '../../util/axios';
+import { MyncmcService } from '../../service/myncmcService';
+import { MyncmcApi } from '../../api/myncmc';
+
+@provide()
+@controller('/api/myncmc')
+export class MyncmcController {
+  @inject()
+  ctx: Context;
+
+  @inject()
+  myncmcService: MyncmcService;
+
+  @post('/login')
+  async login() {
+    const createRule = {
+      username: { type: 'string' },
+      password: { type: 'string' },
+      code: { type: 'string' }
+    };
+
+    // 校验参数
+    this.ctx.validate(createRule);
+
+    const { username, password } = this.ctx.request.body;
+
+    const result = await this.myncmcService.login(username, password);
+    this.ctx.status = result;
+
+    // 保存cookie
+    const cookie = cookieJar.getCookieStringSync(MyncmcApi.loginUrl);
+    this.ctx.logger.info(cookie);
+    this.ctx.session.myncmc = cookie;
+  }
+
+  @post('/logout')
+  async logout() {
+    if (!this.ctx.session.myncmc) {
+      return;
+    }
+
+    this.ctx.status = 200;
+    this.ctx.session.myncmc = undefined;
+  }
+
+  @get('/score', { middleware: ['needMyncmcSession'] })
+  async getScoreReport() {
+    this.ctx.body = await this.myncmcService.getScoreReport();
+  }
+}
